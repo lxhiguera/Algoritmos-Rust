2abc3214443b1f0d520d20a7137f081cf41ba616
Roberto Lozada <robertojoselozada@gmail.com>
2019-05-28 20:27:12 -0500

    Add files via upload

---
+++ UsoMatch.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let x = 1;
+    
+    match x{
+        1 => println!("Uno"),
+        _ => println!("lo que sea"),
+    }
+
+
+}
---
+++ desctructuracion.rs
@@ -0,0 +1,14 @@
+
+fn main(){
+struct Punto{
+    x: i32,
+    y: i32,
+}
+
+let origen = Punto{x: 0, y: 0};
+
+match origen{
+    Punto{x, y} => println!("({},{})", x, y),
+}
+
+}
