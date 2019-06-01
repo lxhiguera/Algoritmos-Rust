132f1b3ac4417c5a68ebf1fabc0f8402b46e208d
Roberto Lozada <robertojoselozada@gmail.com>
2019-05-31 21:16:27 -0500

    Update main.rs

--- gtk/src/main.rs
+++ gtk/src/main.rs
@@ -1,7 +1,7 @@
 extern crate gtk;
 
 use gtk::prelude::*;
-use gtk::{Window, WindowType, Label};
+use gtk::{Window, WindowType, Label, Entry, Box as GtkBox, Orientation};
 
 fn main() {
     if gtk::init().is_err(){
@@ -13,8 +13,21 @@ fn main() {
     window.connect_delete_event(|_,_|{gtk::main_quit(); Inhibit(false) });
     window.set_title("Beta");
     window.set_default_size(300, 300);
-    let label = Label::new(Some("Cualquier cosa"));
-    window.add(&label);
+    let label = Label::new(Some("Texto"));
+    
+    // Creando VBox con 10px de espacio
+    let bx = GtkBox::new(Orientation::Vertical, 10);
+    let entry = Entry::new();
+
+    //Conectando la señal de activación a la funcion anonima
+    entry.connect_activate(|x| println!("{}",x.get_text().unwrap() ));
+
+    //Añadiendo al label
+    bx.pack_start(&label, false, false, 0);
+    bx.pack_start(&entry, false, false, 0);
+
+
+    window.add(&bx);
     window.show_all();
     gtk::main();
 }
